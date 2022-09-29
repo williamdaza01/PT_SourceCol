b814c4c91ff01399ae628aa0abcfbd5367dfa076
Daza <williamdaza04@gmail.com>
2022-09-28 22:28:06 -0500

    Se crea la clase Carro

---
+++ ptSourceCol/Carro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ptSourceCol
+{
+    internal class Carro
+    {
+        public string brand;
+        public string color;
+        public int model;
+
+        public Carro(string brand, string color, int model)
+        {
+            this.brand = brand;
+            this.color = color;
+            this.model = model;
+        }
+    }
+}
