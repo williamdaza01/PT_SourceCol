1a26f72808ad9e696ee30380df6bc08bbd7e939d
Daza <williamdaza04@gmail.com>
2022-09-28 20:14:48 -0500

    Se realiza el punto 1

--- ptSourceCol/Program.cs
+++ ptSourceCol/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ingrese una cadena de texto");
+string str = Console.ReadLine();
+
+
+string puntoUno(string cadena)
+{
+    string aux = "";
+    if (cadena.Length < 20)
+    {
+        return "La cadena: " + cadena + " no tiene mas de 20 caracteres";
+    }
+    else
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            aux += cadena[i];
+        }
+        return aux + "...";
+    }
+}
+
+Console.WriteLine(puntoUno(str));
